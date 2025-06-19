cac9f513475a6af8ce1a2f70368f3f7888631804
Ivan Sapozhnikov <ivan101454@gmail.com>
2025-06-19 19:53:34 +0300

    test problem is done

--- src/main/java/com/example/java_ifortex_test_task/repository/SessionRepository.java
+++ src/main/java/com/example/java_ifortex_test_task/repository/SessionRepository.java
@@ -4,14 +4,35 @@ import com.example.java_ifortex_test_task.entity.DeviceType;
 import com.example.java_ifortex_test_task.entity.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SessionRepository extends JpaRepository<Session, Long> {
-    @Query(value = "", nativeQuery = true)
-    Session getFirstDesktopSession(DeviceType deviceType);
+    /**
+     * Method retrieves the first(earliest) desktop Session
+     * @param deviceType ENUM type of users device (DeviceType.DESKTOP)
+     * @return the session entity
+     */
+    @Query(value = "SELECT s.id, s.device_type - 1 AS device_type, s.ended_at_utc, s.started_at_utc, s.user_id " + //correct output code from code in database to a deviceType
+                           "FROM sessions s " +
+                           "WHERE s.device_type = :deviceType + 1 " + //correct input code from a deviceType to code in database
+                           "ORDER BY s.started_at_utc " +
+                           "LIMIT 1", nativeQuery = true)
+    Session getFirstDesktopSession(@Param("deviceType") DeviceType deviceType);
 
-    @Query(value = "", nativeQuery = true)
-    List<Session> getSessionsFromActiveUsersEndedBefore2025(LocalDateTime endDate);
+    /**
+     * Method retrieves a list of sessions that were ended before 2025 with active users
+     * @param endDate input 2025 year in the LocalDateTime format
+     * @return list of session entities
+     */
+    @Query(value = "SELECT s.id, s.device_type - 1 AS device_type, s.ended_at_utc, s.started_at_utc, s.user_id " + //correct output code from code in database to a deviceType
+                   "FROM sessions s " +
+                   "INNER JOIN users u " +
+                   "ON s.user_id = u.id " +
+                   "WHERE s.ended_at_utc IS NOT NULL AND s.ended_at_utc < :endDate " +
+                   "AND u.deleted = false " +
+                   "ORDER BY s.started_at_utc DESC", nativeQuery = true)
+    List<Session> getSessionsFromActiveUsersEndedBefore2025(@Param("endDate") LocalDateTime endDate);
 }
--- src/main/java/com/example/java_ifortex_test_task/repository/UserRepository.java
+++ src/main/java/com/example/java_ifortex_test_task/repository/UserRepository.java
@@ -4,13 +4,33 @@ import com.example.java_ifortex_test_task.entity.DeviceType;
 import com.example.java_ifortex_test_task.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query(value = "", nativeQuery = true)
-    List<User> getUsersWithAtLeastOneMobileSession(DeviceType deviceType);
+    /**
+     * Method retrieve users who have at least one mobile session
+     * @param deviceType ENUM type of users device (DeviceType.MOBILE)
+     * @return list of users who have at least one mobile session
+     */
+    @Query(value = "SELECT u.* FROM users u " +
+                   "INNER JOIN sessions s " +
+                   "ON u.id = s.user_id " +
+                   "WHERE s.device_type = :deviceType + 1 " + //correct input code from a deviceType to code in database
+                   "GROUP BY u.id, u.first_name, u.last_name, u.middle_name, u.email, u.deleted " +
+                   "ORDER BY MAX(s.started_at_utc) DESC", nativeQuery = true)
+    List<User> getUsersWithAtLeastOneMobileSession(@Param("deviceType") DeviceType deviceType);
 
-    @Query(value = "", nativeQuery = true)
+    /**
+     * Method retrieves a user with the highest number of session
+     * @return a user entity
+     */
+    @Query(value = "SELECT u.* FROM users u " +
+                   "INNER JOIN sessions s " +
+                   "ON u.id = s.user_id " +
+                   "GROUP BY u.id " +
+                   "ORDER BY COUNT(s.id) DESC " +
+                   "LIMIT 1", nativeQuery = true)
     User getUserWithMostSessions();
 }
